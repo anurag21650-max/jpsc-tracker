698482ba02824c88eebc68bb9573f54c4b297af0
anurag21650-max <anurag21650@gmail.com>
2026-06-12 22:59:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.body.innerHTML += "<p>Tracker Loaded</p>";
+fetch("data/notices.json")
+  .then(response => response.json())
+  .then(data => {
+    const notices = document.getElementById("notices");
+
+    data.forEach(item => {
+      notices.innerHTML += `
+        <p>
+          <a href="${item.link}" target="_blank">
+            ${item.title}
+          </a>
+        </p>
+      `;
+    });
+  });
