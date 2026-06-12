86877ecca1fd9bd0e48a01add480dc57c52b489c
anurag21650-max <anurag21650@gmail.com>
2026-06-12 23:03:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-fetch("data/notices.json")
-  .then(response => response.json())
-  .then(data => {
-    const notices = document.getElementById("notices");
-
-    data.forEach(item => {
-      notices.innerHTML += `
-        <p>
-          <a href="${item.link}" target="_blank">
-            ${item.title}
-          </a>
-        </p>
-      `;
-    });
-  });
+document.getElementById("notices").innerHTML =
+  '<p><a href="https://www.jpsc.gov.in" target="_blank">JPSC Tracker Started</a></p>';
