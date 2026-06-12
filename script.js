dc33e6de7a45597f77a66c4a9c4754069cce32df
anurag21650-max <anurag21650@gmail.com>
2026-06-12 23:05:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-document.getElementById("notices").innerHTML =
-  '<p><a href="https://www.jpsc.gov.in" target="_blank">JPSC Tracker Started</a></p>';
+fetch("./data/notices.json")
+  .then(r => r.json())
+  .then(data => {
+    const notices = document.getElementById("notices");
+
+    data.forEach(item => {
+      const p = document.createElement("p");
+
+      p.innerHTML =
+        `<a href="${item.link}" target="_blank">${item.title}</a>`;
+
+      notices.appendChild(p);
+    });
+  })
+  .catch(err => {
+    document.getElementById("notices").innerHTML =
+      "<p>Error loading notices.</p>";
+    console.error(err);
+  });
